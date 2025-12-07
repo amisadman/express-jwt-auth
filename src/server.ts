30dd95bb4c5af399b28178c6a75ee52e84dbcd01
amisadman <inboxsadman@gmail.com>
2025-12-07 19:25:28 +0600

    feat: added modular pattern for db

--- src/server.ts
+++ src/server.ts
@@ -3,48 +3,17 @@ import { Pool, Result } from "pg";
 import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import config from "./config";
+import initDb, { pool } from "./config/db";
+
 
 const app = express();
-const port = 5000;
+const port = config.port;
 //parser
 app.use(express.json());
 // app.use(express.urlencoded());
 
-//db
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STRING}`,
-});
 
-const initDb = async () => {
-  await pool.query(`
-            CREATE TABLE IF NOT EXISTS users(
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(150) UNIQUE NOT NULL,
-            age INT,
-            address TEXT,
-            phone VARCHAR(20),
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-        `);
-
-  await pool.query(
-    `CREATE TABLE IF NOT EXISTS todos(
-             id SERIAL PRIMARY KEY,
-             userid INT REFERENCES users(id) ON DELETE CASCADE,
-             title VARCHAR(200) NOT NULL,
-             description TEXT,
-             completed BOOLEAN DEFAULT false,
-             due_date DATE,
-             created_at TIMESTAMP DEFAULT NOW(),
-             updated_at TIMESTAMP DEFAULT NOW()
-
-            )
-            `
-  );
-};
 
 initDb();
 //logger
